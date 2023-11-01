b016b19aad282d84e50ba37f5ad6af2fd0d34053
Khalid <khalid20031016@gmail.com>
2023-11-01 18:35:38 +0500

    login form added

--- project/src/App.js
+++ project/src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
-import logo from './assets/icons/logo_white.svg'
-import logo_black from './assets/icons/logo_black.png'
-import {Sidebar} from './components/Sidebar';
-import {SellerHeader} from './components/SellerHeader';
-// import { AddProduct } from './components/AddProduct';
-import { GlobalContext } from './context/GlobalContext';
-import { ProductList } from './components/ProductList';
+// import logo from './assets/icons/logo_white.svg'
+// import logo_black from './assets/icons/logo_black.png'
+// import {Sidebar} from './components/Sidebar';
+// import {SellerHeader} from './components/SellerHeader';
+// // import { AddProduct } from './components/AddProduct';
+// import { GlobalContext } from './context/GlobalContext';
+// import { ProductList } from './components/ProductList';
+
+// function App() {
+//   return (
+//     <GlobalContext>
+//           <SellerHeader image={logo} />
+//           <div className='flex-container'>
+//             <Sidebar image={logo_black}/>
+//             <ProductList />
+//           </div>
+//     </GlobalContext>
+//   );
+// }
+
+// export default App;
+import LoginSignup from './components/Loginsignup';
 
 function App() {
   return (
-    <GlobalContext>
-          <SellerHeader image={logo} />
-          <div className='flex-container'>
-            <Sidebar image={logo_black}/>
-            <ProductList />
-          </div>
-    </GlobalContext>
+    <div >
+      <LoginSignup/>
+    </div>
   );
 }
 
